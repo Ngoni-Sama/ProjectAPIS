1062ddcadb7bf84023120eaabc1f3d1ba8a6aa44
raguirre <raguirre00@gmail.com>
2016-04-26 13:15:16 -0500

    Alpha release

--- app/js/play.js
+++ app/js/play.js
@@ -1,7 +1,12 @@
-app.controller("PlayCtrl", function($scope, $stateParams){
+app.controller("PlayCtrl", function($scope, $state, $stateParams, $mdDialog){
+
+    if ($stateParams.juice == null) {
+        $state.go("/");
+    }
 
     var juicePH = $stateParams.juice.juicePH;
     var juiceLabels = $stateParams.juice.juiceLabels;
+    var juiceEqPoint = $stateParams.juice.juiceEqPoint;
 
     console.log($stateParams);
 
@@ -9,13 +14,28 @@ app.controller("PlayCtrl", function($scope, $stateParams){
     $scope.series = ['pH'];
     $scope.data = [ [] ];
     $scope.increaseDisabled = false;
+    $scope.bottle = "/app/img/chemistry-flash-hi.png";
+    $scope.currentPH = juicePH[juicePH.length-1];
 
     $scope.increase = function(){
-        $scope.data[0].push(juicePH.pop());
+        var value = juicePH.pop();
+        $scope.currentPH = value;
+        $scope.data[0].push(value);
         $scope.labels.push(juiceLabels.pop());
         console.log(juicePH.length);
         if(juicePH.length === 0){
             $scope.increaseDisabled = true;
+        }
+        if(value == juiceEqPoint){
+            $scope.bottle = "/app/img/chemistry-flash-blue.png"
+            $mdDialog.show(
+                $mdDialog.alert()
+                    .parent(angular.element(document.querySelector('#body')))
+                    .clickOutsideToClose(false)
+                    .title('Punto de Equivalencia = ' + juiceEqPoint)
+                    .textContent('¡Hemos llegado al punto de equivalencia! YESSS (aquí va una explicación mejor que aún no está pero va a estar)')
+                    .ok('Aceptar')
+                );
         }
     };
 
