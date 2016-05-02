f0d6dbdc7346a11f7bb8804e010d4d532e55003b
raguirre <raguirre00@gmail.com>
2016-05-02 17:47:10 -0500

    New dialog prototype

--- app/js/play.js
+++ app/js/play.js
@@ -22,22 +22,49 @@ app.controller("PlayCtrl", function($scope, $state, $stateParams, $mdDialog){
         $scope.currentPH = value;
         $scope.data[0].push(value);
         $scope.labels.push(juiceLabels.pop());
-        console.log(juicePH.length);
         if(juicePH.length === 0){
             $scope.increaseDisabled = true;
         }
         if(value == juiceEqPoint){
             $scope.bottle = "/app/img/chemistry-flash-blue.png"
-            $mdDialog.show(
-                $mdDialog.alert()
-                    .parent(angular.element(document.querySelector('#body')))
-                    .clickOutsideToClose(false)
-                    .title('Punto de Equivalencia = ' + juiceEqPoint)
-                    .textContent('¡Hemos llegado al punto de equivalencia! YESSS (aquí va una explicación mejor que aún no está pero va a estar)')
-                    .ok('Aceptar')
-                );
+            // $mdDialog.show(
+            //     $mdDialog.alert()
+            //         .parent(angular.element(document.querySelector('#body')))
+            //         .clickOutsideToClose(false)
+            //         .title('Punto de Equivalencia = ' + juiceEqPoint)
+            //         .textContent('¡Hemos llegado al punto de equivalencia! YESSS (aquí va una explicación mejor que aún no está pero va a estar)')
+            //         .ok('Aceptar')
+            //     );
+            showDialog();
         }
     };
+
+    function showDialog() {
+        $mdDialog.show({
+            controller: DialogController,
+            templateUrl: 'app/html/eq_dialog.html',
+            parent: angular.element(document.body),
+            clickOutsideToClose:true,
+        })
+        .then(function(answer) {
+            $scope.status = 'You said the information was "' + answer + '".';
+        }, function() {
+            $scope.status = 'You cancelled the dialog.';
+        });
+    };
+    function DialogController($scope, $mdDialog) {
+        $scope.hide = function() {
+            $mdDialog.hide();
+        };
+        $scope.cancel = function() {
+            $mdDialog.cancel();
+        };
+        $scope.answer = function(answer) {
+            $mdDialog.hide(answer);
+        };
+    }
+
+    showDialog();
 
     $scope.data[0].push(juicePH.pop());
     $scope.labels.push(juiceLabels.pop());
